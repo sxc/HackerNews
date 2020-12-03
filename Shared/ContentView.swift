d15e705e1baa91849b25aca620b5b8e432539df2
sxc <qiuyequ@icloud.com>
2020-12-03 19:22:46 +0800

    Initial the project

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+        List(posts) { post in
+            Text(post.title)
+        }
+        .navigationTitle("Hacker News")
+        }
+        
     }
 }
 
@@ -19,3 +24,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Hola"),
+    Post(id: "3", title: "Benjova")
+]
