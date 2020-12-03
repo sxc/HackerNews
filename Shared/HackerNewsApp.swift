3dedf1c22e30e3c59ac2134af4d44c8321031648
sxc <qiuyequ@icloud.com>
2020-12-03 19:13:32 +0800

    Initial Commit

---
+++ Shared/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  Shared
+//
+//  Created by Xiaochun Shen on 2020/12/3.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
