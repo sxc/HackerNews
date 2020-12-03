bf4d6c8653b796521780cda1dbe7f2df5de4b5ae
sxc <qiuyequ@icloud.com>
2020-12-03 19:59:29 +0800

    add post structure

---
+++ Shared/Models/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  HackerNews (iOS)
+//
+//  Created by Xiaochun Shen on 2020/12/3.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") {
+            let session = URLSession(configuration: .default)
+            
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let results =  try decoder.decode(Results.self, from: safeData)
+                        } catch {
+                            print(error)
+                        }
+                        
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
