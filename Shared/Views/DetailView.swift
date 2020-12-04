eb9cce852e08bd9ac8d359a367b5c4c3a1255425
sxc <qiuyequ@icloud.com>
2020-12-04 09:43:05 +0800

    refactor the web view

--- Shared/Views/DetailView.swift
+++ Shared/Views/DetailView.swift
@@ -31,21 +31,3 @@ struct DetailView_Previews: PreviewProvider {
     }
 }
 
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-            }
-        
-        }
-    }
-}
---
+++ Shared/Views/WebView.swift
@@ -0,0 +1,30 @@
+//
+//  WebView.swift
+//  HackerNews
+//
+//  Created by Xiaochun Shen on 2020/12/4.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) ->WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        
+        }
+    }
+}
+
